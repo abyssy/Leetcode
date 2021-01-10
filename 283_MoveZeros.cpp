f2a6f8fd19a5fea73bc53d42931b6d1ff80da585
abyssy <31658094+abyssy@users.noreply.github.com>
2021-01-10 15:42:01 +0800

    Create 283_MoveZeros.cpp

---
+++ 283_MoveZeros.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int cnt = 0;
+        for ( int i = 0; i < nums.size(); i ++)
+        {
+            if (nums[i] == 0)
+                continue;
+            else
+            {
+                nums[cnt ++] = nums[i];
+            }
+        }
+        for ( ; cnt < nums.size(); cnt ++)
+            nums[cnt] = 0;
+    }
+};
