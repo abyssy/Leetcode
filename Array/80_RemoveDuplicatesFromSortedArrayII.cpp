2220a14daf9db90738656fb2fa2ecbbf401e9882
abyssy <31658094+abyssy@users.noreply.github.com>
2021-01-19 00:08:10 +0800

    Create 80_RemoveDuplicatesFromSortedArrayII.cpp

---
+++ Array/80_RemoveDuplicatesFromSortedArrayII.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    //思路：从头到尾扫描，i是长度，也即重复最多两个元素的下一个位置，即可以存放下一个不同元素的位置
+    //不论重复与否都不断覆盖前面的位置
+    int removeDuplicates(vector<int>& nums) {
+        int flag = 1;
+        if (nums.size() == 0 || nums.size() == 1)
+            return nums.size();
+        int i = 1;
+        int cur = nums[0];
+        for (int j = 1; j < nums.size(); j ++){
+            if (cur == nums[j])
+            {
+                flag ++;
+                if (flag <= 2)
+                    nums[i ++] = nums[j];
+            }
+            else{
+                flag = 1;
+                nums[i ++] = nums[j];
+                cur = nums[j];
+            }
+        }
+        return i;
+    }
+};
+
+//执行用时：8 ms, 在所有 C++ 提交中击败了86.69%的用户
+//内存消耗：10.4 MB, 在所有 C++ 提交中击败了87.69%的用户
