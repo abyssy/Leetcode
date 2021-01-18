0260c381e52e6d5570d0d64874713d1d28b2993b
abyssy <31658094+abyssy@users.noreply.github.com>
2021-01-19 00:10:11 +0800

    Update 80_RemoveDuplicatesFromSortedArrayII.cpp

--- Array/80_RemoveDuplicatesFromSortedArrayII.cpp
+++ Array/80_RemoveDuplicatesFromSortedArrayII.cpp
@@ -3,10 +3,13 @@ public:
     //思路：从头到尾扫描，i是长度，也即重复最多两个元素的下一个位置，即可以存放下一个不同元素的位置
     //不论重复与否都不断覆盖前面的位置
     int removeDuplicates(vector<int>& nums) {
+        //flag指当前元素出现的次数
         int flag = 1;
         if (nums.size() == 0 || nums.size() == 1)
             return nums.size();
+        //i为有效长度
         int i = 1;
+        //cur指当前正在比对哪个元素
         int cur = nums[0];
         for (int j = 1; j < nums.size(); j ++){
             if (cur == nums[j])
